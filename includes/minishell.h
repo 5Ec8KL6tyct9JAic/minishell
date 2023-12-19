55e3dc6b8e1a68e737daf5bd895241219e8b6963
K3K!u71 <105453364+5Ec8KL6tyct9JAic@users.noreply.github.com>
2023-12-19 15:42:22 +0100

    colors.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dvalerio <dvalerio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 12:12:42 by dvalerio          #+#    #+#             */
-/*   Updated: 2023/12/18 22:20:06 by dvalerio         ###   ########.fr       */
+/*   Updated: 2023/12/19 15:36:36 by dvalerio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define MINISHELL_H
 
 # include "../Libft/includes/libft.h"
+# include "color.h"
 # include <string.h>
 # include <fcntl.h>
 # include <sys/stat.h>
